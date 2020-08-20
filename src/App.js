dbb5875514281e9a9e38910013bb6918c40ff1d2
Alfredo Montero <christianmontero08@gmail.com>
2020-08-19 19:56:26 -0700

    Remove logo and unused imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world from React
         </p>
         <p>
-          This is going to be the spendify app
+          I need to finish my CI/CD pipeline
         </p>
       </header>
     </div>
